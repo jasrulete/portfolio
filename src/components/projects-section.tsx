57f3d467127f90eb3084cc3c7458aee77c0685c7
jer2x <rulete.jeric@gmail.com>
2025-05-05 02:21:13 +0800

    adjusted projects

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -1,6 +1,6 @@
 import background from '../assets/background.jpg';
 import dungeon_descent from '../assets/dungeon.png';
-import finance_app from '../assets/finance.png';
+import finance from '../assets/finance.png'
 
 function Projects() {
   const projects = [
@@ -23,19 +23,19 @@ function Projects() {
     {
       title: "Finance App",
       description: "A finance app that allows users to track their expenses and income.",
-      image: finance_app,
+      image: finance,
       tags: ["HTML", "CSS", "JavaScript"],
       github: "https://github.com/jasrulete/finance-app",
       demo: "#",
     },
-    {
-      title: "Finance App",
-      description: "A finance app that allows users to track their expenses and income.",
-      image: finance_app,
-      tags: ["HTML", "CSS", "JavaScript"],
-      github: "https://github.com/jasrulete/finance-app",
-      demo: "#",
-    },
+    // {
+    //   title: "Finance App",
+    //   description: "A finance app that allows users to track their expenses and income.",
+    //   image: finance_app,
+    //   tags: ["HTML", "CSS", "JavaScript"],
+    //   github: "https://github.com/jasrulete/finance-app",
+    //   demo: "#",
+    // },
   ]
 
   return (
