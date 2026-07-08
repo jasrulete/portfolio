0946351c97c7f860e8460f8910fc14fa3101859c
jasrulete <rulete.jeric@gmail.com>
2026-07-09 02:44:26 +0800

    updated aboutapp in desktop mode

--- src/components/desktop/apps/AboutApp.tsx
+++ src/components/desktop/apps/AboutApp.tsx
@@ -2,7 +2,14 @@ import AboutSection from "../../about-section";
 
 export default function AboutApp() {
   return (
-    <div className="[&_section]:!py-6 [&_section]:!bg-transparent">
+    <div
+      className="
+        [&_section]:!py-6 [&_section]:!bg-transparent
+        [&_.grid]:!grid-cols-1
+        [&_img]:!w-32 [&_img]:!h-32
+        [&_.max-w-7xl]:!max-w-none
+      "
+    >
       <AboutSection />
     </div>
   );
